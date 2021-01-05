71490cadccd2f8a272cb696f045f8511a562ef77
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-06 00:04:52 +0630

    Set v2.2.2

--- src/app/shared/app-settings.ts
+++ src/app/shared/app-settings.ts
@@ -1,7 +1,7 @@
 import { AppConfig } from './app-config';
 
 export const appSettings: AppConfig = {
-    appVersion: '2.2.1',
+    appVersion: '2.2.2',
     appName: 'Unicode Code Point Lookup',
     appDescription:
         'Unicode Code Point Lookup is a Unicode encode decode online tool to convert Unicode codepoint numbers to text and vice versa by DagonMetric Myanmar Tools.',
