9703399ea85463f02110e340447593ab4b153dae
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-05 23:10:45 +0630

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -14,27 +14,30 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-            require('karma-junit-reporter')
+            require('karma-coverage'),
+            require('karma-junit-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+            },
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, './dist/coverage'),
-            reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
-            fixWebpackSourcePaths: true,
-            thresholds: {
-                statements: 80,
-                lines: 80,
-                branches: 80,
-                functions: 80
-            }
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage'),
+            subdir: '.',
+            reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }, { type: 'cobertura' }]
         },
         reporters: ['progress', 'kjhtml'],
         junitReporter: {
-            outputDir: './dist/junit'
+            outputDir: './junit'
         },
         port: 9876,
         colors: true,
